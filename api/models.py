4ae0eca0dcc01b7f2dfc910df6c18dd36c05f012
Marinel444 <380978907444m@gmail.com>
2024-03-21 18:58:08 +0200

    added upload_location for Post.image && changed authorization using email and create User model

--- api/models.py
+++ api/models.py
@@ -1,14 +1,22 @@
+import os
+
 from django.conf import settings
 from django.db import models
 
 
+def upload_location(instance, filename):
+    return f"posts/{instance.author.username}/{filename}"
+
+
 class Post(models.Model):
     description = models.TextField()
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE
+    )
     likes = models.ManyToManyField(
         settings.AUTH_USER_MODEL, related_name="liked_posts", blank=True
     )
-    image = models.ImageField(upload_to=f"posts/%Y/%m/%d/", blank=True)
+    image = models.ImageField(upload_to=upload_location, blank=True)
     updated_at = models.DateTimeField(auto_now=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
--- user/models.py
+++ user/models.py
@@ -1,6 +1,48 @@
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
+from django.utils.translation import gettext as _
+
+
+class UserManager(BaseUserManager):
+    use_in_migrations = True
+
+    def _create_user(self, email, username, password, **extra_fields):
+        """Create and save a User with the given email, username, and password."""
+        if not email:
+            raise ValueError(_("The given email must be set"))
+        if not username:
+            raise ValueError(_("The given username must be set"))
+        email = self.normalize_email(email)
+        user = self.model(email=email, username=username, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, username, password=None, **extra_fields):
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, username, password, **extra_fields)
+
+    def create_superuser(self, email, username, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError(_("Superuser must have is_staff=True."))
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError(_("Superuser must have is_superuser=True."))
+
+        return self._create_user(email, username, password, **extra_fields)
+
+
+def user_directory_path(instance, filename):
+    return f"users/{instance.username}/{filename}"
 
 
 class User(AbstractUser):
-    pass
+    email = models.EmailField(_("email address"), unique=True)
+    bio = models.TextField(blank=True)
+    image = models.ImageField(upload_to=user_directory_path, blank=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["username"]
