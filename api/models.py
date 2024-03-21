5c54ae4a2dba5767770ef356876730a0537ad38f
Marinel444 <380978907444m@gmail.com>
2024-03-21 08:22:32 +0200

    Created models Post and Comment

--- api/models.py
+++ api/models.py
@@ -1,3 +1,26 @@
+from django.conf import settings
 from django.db import models
 
-# Create your models here.
+
+class Post(models.Model):
+    description = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    likes = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="liked_posts")
+    image = models.ImageField(upload_to=f"posts/%Y/%m/%d/")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return f"{self.author} - {self.created_at}"
+
+
+class Comment(models.Model):
+    text = models.TextField()
+    author = models.ForeignKey(
+        settings.AUTH_USER_MODEL, related_name="comments", on_delete=models.CASCADE
+    )
+    post = models.ForeignKey(Post, related_name="comments", on_delete=models.CASCADE)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
